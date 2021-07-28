e7bfbd29e5d4701470a399fa1ea0c449f81e9324
Kevin <kmancxc@gmail.com>
2021-07-27 22:19:43 -0700

    param must be the first thing in the file. also naming the file was annoying so you dont anymore

--- scripts/file_transfer/wget.ps1
+++ scripts/file_transfer/wget.ps1
@@ -1,8 +1,8 @@
-$curDir = $pwd
 param (
-    [Parameter(Mandatory=$true)][string]$url,
-    [Parameter(Mandatory=$true)][string]$outfile
+    [Parameter(Mandatory=$true)][string]$url
 )
+$curDir = $pwd
+$outfile = $url.Split("/")[-1]
 $output = -join($curDir, "\", $outfile)
 $webclient = New-Object System.Net.WebClient
 $webclient.DownloadFile($url, $output)
