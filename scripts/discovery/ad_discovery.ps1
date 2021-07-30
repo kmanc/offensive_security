7c89b7755203d59b52c73142e4e2faac5f442b9a
Kevin <kmancxc@gmail.com>
2021-07-30 09:31:51 -0700

    template for ad discovery script with stubs in comments

---
+++ scripts/discovery/ad_discovery.ps1
@@ -0,0 +1,42 @@
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+$PDC = ($domainObj.PdcRoleOwner).Name
+$searchString = "LDAP://"
+$searchString += $PDC + "/"
+$distinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+$searchString += $distinguishedName
+$searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$searchString)
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+$searcher.SearchRoot = $objDomain
+# To get all accounts and their properties
+<#
+$searcher.filter = "samAccountType=805306368"
+$Result = $searcher.FindAll()
+Foreach($obj in $Result)
+{
+    Foreach($prop in $obj.Properties)
+    {
+        $prop
+    }
+    Write-Host "-------------------------"
+}
+#>
+
+# To get all groups
+<#
+$searcher.filter = "objectClass=Group"
+$Result = $searcher.FindAll()
+Foreach($obj in $Result)
+{
+    $obj.Properties.Name
+}
+#>
+
+# To get all members of a group
+<#
+$searcher.filter = "name=GroupName"
+$Result = $searcher.FindAll()
+Foreach($obj in $Result)
+{
+    $obj.Properties.member
+}
+#>
