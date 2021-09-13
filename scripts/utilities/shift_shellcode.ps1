82b3d8b718adfc8869603cfe8a98933df0555f6d
kmanc <kmancxc@gmail.com>
2021-09-13 11:01:51 -0700

    updated and tested in powershell

--- scripts/utilities/shift_shellcode.ps1
+++ scripts/utilities/shift_shellcode.ps1
@@ -6,7 +6,14 @@ $buf = [Byte[]] (0x41, 0x41, 0x41)
 $ciphertext = New-Object System.Collections.Generic.List[System.Object]
 # Iterate over each byte
 foreach ($byte in $buf) {
-    $ciphertext.Add(($byte + $key) % 255)
+    # Shift the byte
+    $xored = ($byte + $key) % 255
+    # Format the byte as a left-padded hex string with a leading "0x" and a trailing ","
+    $formatted = "0x" + [System.Convert]::ToString($xored, 16).PadLeft(2, "0") + ","
+    # Add it to the output
+    $ciphertext.Add($formatted)
 }
+# Remove the comma from the last byte
+$ciphertext[-1] = $ciphertext[-1].Substring(0, 4)
 # Write the answer
 Write-Host $ciphertext
